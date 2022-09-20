5b81b48074ec8adedb0883b7724c5544b043d458
taispooja <113901153+taispooja@users.noreply.github.com>
2022-09-20 13:43:15 +0530

    Update RoleRepository.java

--- user-management/src/main/java/com/usermanagement/repository/RoleRepository.java
+++ user-management/src/main/java/com/usermanagement/repository/RoleRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.usermanagement.model.entities.Role;
@@ -19,5 +21,8 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
 	Set<Role> findByNameIn(List<String> roleNames);
 
 	Role findByIsDefaultTrue();
+
+	@Query("select case when count(r)> 0 then true else false end from Role r where r.name= :name and id != :id")
+	boolean existsByIdAndName(@Param("id") Long id, @Param("name") String name);
 
 }
